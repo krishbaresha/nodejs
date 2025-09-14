842c1213d5abc8d5f688e287935de4c86bad94e4
krishbaresha <krishbaresha@gmail.com>
2025-09-14 17:44:24 +0500

    Refactor route handling in index.js for improved clarity and organization

--- Project/index.js
+++ Project/index.js
@@ -7,8 +7,21 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname,"Public"))) //console.log(__dirname) ==> E:\.JS.PY\Javascript\nodejs\Project
 app.set('view engine', 'ejs')
 
+// app.get('/',(req,res)=>{
+//     res.render("index")
+// })
+
 app.get('/',(req,res)=>{
-    res.render("index")
+    res.render("index");
+
+})
+app.get('/profile/:username',(req,res)=>{
+    req.params.username;
+    res.send(`Welcome, ${req.params.username}`);
+})
+app.get('/author/:username/:age',(req,res)=>{
+    res.send(`Welcome, ${req.params.username} Your Age is ${req.params.age}`);
+    // res.send(req.params);
 })
 
 app.listen(3000,()=>{
